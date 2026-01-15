cf07f59734f6c8ffe00850d5c70f1ff5fe028532
florstrd <florstrd@arcada.fi>
2026-01-15 12:52:59 +0200

    angular task 4 & 5

--- first-project/first-project/src/app/weather/weather.ts
+++ first-project/first-project/src/app/weather/weather.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { WeatherYesterday } from '../weather-yesterday/weather-yesterday';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [WeatherYesterday],
   templateUrl: './weather.html',
   styleUrl: './weather.css',
 })
 export class Weather {
   protected readonly temperature = "-20";
   protected readonly description = "its cloudy";
+  protected readonly yesterday = "Yesterdays weather was -15 and it was cloudy";
 }
